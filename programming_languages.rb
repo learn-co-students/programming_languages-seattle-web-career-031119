27395d5b2e0eb033d45554af3465fcaaf12069ac
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-17 18:23:22 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  result = {}
+  languages.each do |style, langs|
+    langs.each do |lang, types|
+      if(result[lang])
+        result[lang][:style] << style
+      else 
+        result[lang] = {
+          type: types.values.join(""),
+          style: [] << style
+        }
+      end
+    end
+  end
+  result
 end
